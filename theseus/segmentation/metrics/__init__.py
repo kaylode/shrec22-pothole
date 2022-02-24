222ebe7d04a0497b96e0b580e4eba836884a8eb4
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-02-24 23:50:13 +0700

    finalize metrics"

--- theseus/segmentation/metrics/__init__.py
+++ theseus/segmentation/metrics/__init__.py
@@ -2,7 +2,8 @@ from theseus.base.metrics import METRIC_REGISTRY
 
 from .dicecoeff import *
 from .pixel_accuracy import *
-
+from .mean_iou import *
 
 METRIC_REGISTRY.register(PixelAccuracy)
 METRIC_REGISTRY.register(DiceScore)
+METRIC_REGISTRY.register(mIOU)
