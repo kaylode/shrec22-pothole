498ce104b471f3d1d6082300acac0ab2a305ac05
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2022-03-01 19:40:08 +0700

    add inference shell scripts

---
+++ tools/submission01/infer_video.sh
@@ -0,0 +1,17 @@
+# ./tools/submission_1_video.sh data/a-20220203-085153-color-c000169.mp4 cpu 
+CHECKPOINT='ckpt/unet/best.pth'
+CONFIG_TRANSFORM='ckpt/unet/transform.yml'
+SAVE_DIR='./output/submission01'
+CONFIG_TEST='tools/submission01/test_video.yml'
+CLS_TXT='tools/classes.txt'
+
+mkdir -p $SAVE_DIR
+
+python  configs/segmentation/infer_video.py \
+        -c $CONFIG_TEST \
+        -o global.save_dir=$SAVE_DIR \
+        global.device=$2 \
+        global.cfg_transform=$CONFIG_TRANSFORM \
+        global.weights=$CHECKPOINT \
+        data.dataset.args.txt_classnames=$CLS_TXT \
+        data.dataset.args.video_path=$1
